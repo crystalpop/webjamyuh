23abb5154f06a97e100c7fa313e77a8055d15657
sophiabtran <150560518+sophiabtran@users.noreply.github.com>
2023-11-30 18:03:26 -0800

    app.js data updated

---
+++ frontend/webjam-frontend/src/End.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './End.css';
+import { NavLink, useNavigate } from 'react-router-dom';
+import {useState} from 'react';
+
+function End() {
+  return (
+    <body>
+    </body>
+  );
+}
+
+export default End;
--- frontend/webjam-frontend/src/Main.jsx
+++ frontend/webjam-frontend/src/Main.jsx
@@ -3,22 +3,18 @@ import { Route, Routes, Navigate, BrowserRouter } from "react-router-dom"
 import './App.css'
 import App from './App.js'
 import Game from './Game.js'
-import End from './End.js'
 import { ThemeProvider } from 'react-bootstrap'
 
 function Main() {
  
 
   return (
-    <ThemeProvider>
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<App />} />
         <Route path='/game' element={<Game />} />
-        <Route path='/end' element={<End />} />
       </Routes>
       </BrowserRouter>
-    </ThemeProvider>
   );
 }
 
